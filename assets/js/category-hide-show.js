c116c040870d1ce1c2e0aff398e31db74901441b
Kunj <kunj.webiots@gmail.com>
2025-11-08 16:13:46 +0530

    changes

--- assets/js/category-hide-show.js
+++ assets/js/category-hide-show.js
@@ -48,3 +48,27 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.querySelector(".categoryButton");
+
+    btn.addEventListener("click", function () {
+        const homeSection = document.querySelector(".product-banner-section");
+        const left = homeSection.querySelector(".custom-col-xxl-3");
+        const right = homeSection.querySelector(".col-12, .custom-col-xxl-9");
+
+        if (left && right) {
+            if (left.style.display === "none") {
+                left.style.display = "";
+                right.classList.remove("col-12");
+                right.classList.add("custom-col-xxl-9");
+                console.log("Left shown & Right normal");
+            } else {
+                left.style.display = "none";
+                right.classList.remove("custom-col-xxl-9");
+                right.classList.add("col-12");
+                console.log("Left hidden & Right expanded");
+            }
+        }
+    });
+});
